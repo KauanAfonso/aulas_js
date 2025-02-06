10358bc20c8c576e3f3c52dabc88719d656e3039
= <=>
2025-02-06 10:12:06 -0300

    aula 01

---
+++ aula1/index.js
@@ -0,0 +1,21 @@
+//Alert - exibe recados para meu querido suário,onde ele 
+//resumindo quem mandou sou eu
+window.alert("Olá mundo, estamos quebrando uma maldição")
+alert("Já estava com saudades de Vocês !")
+
+//prompt - é o input do python
+//Serve para obter respostas do meu querido usuário
+// Let: valore que podem ser adiquirida dos usuários durante o uso da aplicação, 
+// const: valores imutavel, 
+// var: é a forma legada de declarar variaveis, porem ainda visto em sistemas legados. 
+let materia = prompt("Qual sua materia favorita? ")
+document.body.innerHTML += `<p>${materia}</p>`
+
+
+
+
+
+//confirm - serve para permitir escolha 
+confirm("Ainda está aqui? ")
+
+console.log(materia)
