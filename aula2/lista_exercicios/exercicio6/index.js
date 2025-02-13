1fbeecdf3b3f0c5099b0180031bbc7b3d46e026c
= <=>
2025-02-13 11:05:34 -0300

    exercicio 07

--- aula2/lista_exercicios/exercicio6/index.js
+++ aula2/lista_exercicios/exercicio6/index.js
@@ -4,12 +4,12 @@ const calcular_media = (nota1, nota2) =>{
     let resultado = (media > 6.0)? "Acima da média" : "Não acima da média"
     let par_impar = (media % 2 == 0) ? "par" : "Impar"
 
-    alert(resultado + "Sua média é : " + par_impar)
+    alert(resultado + "Sua média é : " +media + " então: "+ par_impar)
 }
 
 alert('Programa que retorna se a media está acima e se ela é par ou impar')
-let nota1 = Number(prompt("Digite a nota1 "))
-let nota2 = Number(prompt("Digite a nota2 "))
+let nota1 = Number(prompt("Digite a nota 1: "))
+let nota2 = Number(prompt("Digite a nota 2: "))
 
 calcular_media(nota1,nota2)
 
---
+++ aula2/lista_exercicios/exercicio7/index.js
@@ -0,0 +1,34 @@
+
+
+const verificar_imc = (imc)=>{
+    if(imc < 18.5){
+        return "Baixo"
+    }
+    else if(imc>=18.5 && imc <= 24.9){
+        return "Normal"
+    }    
+    else if(imc>=25 && imc <= 29.9){
+        return "Sobrepeso"
+    }
+    else if(imc>=30 && imc <= 34.9){
+        return "Obesidade I"
+    }
+    else if(imc>=35 && imc <= 39.9){
+        return "Obesidade II"
+    }else{
+        return "Obesidde III"
+    }
+}
+
+
+const calcular_imc = (peso, altura) =>{
+    let imc = peso / (altura * altura)
+    return verificar_imc(imc)
+}
+
+alert('Programa que retorna imc')
+let peso = Number(prompt("Digite seu peso "))
+let altura = Number(prompt("Digite sua altura "))
+
+alert(calcular_imc(peso,altura))
+
