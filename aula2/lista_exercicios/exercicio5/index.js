5b3a42592925e8b6a4e0310e593377f59e8d2986
= <=>
2025-02-13 10:26:34 -0300

    exercicio05

---
+++ aula2/lista_exercicios/exercicio5/index.js
@@ -0,0 +1,9 @@
+const calcular_area = (base, altura) =>{
+    return base * altura
+}
+
+alert('Programa que retorna grau celsius em Fahrenheint ')
+let base = Number(prompt("Digite a base "))
+let altura = Number(prompt("Digite a altura "))
+alert("Area de: , "+ calcular_area(base,altura))
+
