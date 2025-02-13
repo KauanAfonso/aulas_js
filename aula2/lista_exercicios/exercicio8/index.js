a7942e8281b2658d117732c40205be3ed22faad6
kauan <kauanafon3@gmail.com>
2025-02-13 11:14:31 -0300

    exercicio 8

---
+++ aula2/lista_exercicios/exercicio8/index.js
@@ -0,0 +1,13 @@
+
+
+const verificar_ano = (ano)=>{
+
+    let resultado =(ano % 4 == 0 && ano % 100 !== 0) ? "Ano bissexto" : "Não é bisesexto"
+    return resultado
+
+}
+
+alert("Programa que verifica se um ano é bissexto")
+let ano = Number(prompt("Digite o ano: (ex: 2024)"))
+alert(verificar_ano(ano)) 
+
