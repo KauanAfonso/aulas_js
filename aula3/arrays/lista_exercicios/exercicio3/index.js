99115e562d9757a5e465cbe7414910f5134e4e38
kauanAfonso <kauanafon3@gmail.com>
2025-03-13 11:08:46 -0300

    exercicio3

--- aula3/arrays/lista_exercicios/exercicio3/index.js
+++ aula3/arrays/lista_exercicios/exercicio3/index.js
@@ -3,4 +3,27 @@
 e apresente as idades acima da média.
 */
 
-const media_idades 
+const media_aritimetica = (lista) => {
+    let media = lista.reduce((acumulador,valor) => {
+        return acumulador+=valor
+    },0)
+    
+    return media / lista.length
+}
+
+
+const media_idades = (idades) =>{
+    let media = media_aritimetica(idades)
+    let contador = 0
+
+    console.log(`media de idades: ${media}`)
+    idades.forEach(idade => {
+        (idade > media) ?contador+=1:contador
+    });
+
+    return contador
+}
+
+let idades = [8,15,66,40,2,9,34,18]
+console.log(idades)
+console.log(media_idades(idades) + " Pessoa(s) tem a idade maior que a media de idades")
