a124ce1f942293c55091beda98f2b08e6c24848c
kauanAfonso <kauanafon3@gmail.com>
2025-03-27 11:27:50 -0300

    Falta terminar o 09

---
+++ aula3/arrays/lista_exercicios/exercicio9/index.js
@@ -0,0 +1,23 @@
+/* 
+
+Desenvolva um programa que leia 5 números inteiros e armazene em um vetor A. 
+Leia 5 números inteiros e armazene em um vetor B. 
+Carregue e apresente um vetor C com os valores de A e B alternados. 
+
+
+*/
+
+
+const alternar_vetores = (vetor1, vetor2) => {
+    vetor3 = []
+    
+}   
+
+let vetor1 = [1, 2, 3, 4, 5]
+let vetor2 = [6, 7, 8, 9, 10]
+let numero
+do {
+    
+}while()
+
+alert(`Primeiro vetor: ${minha_lista}\n Vetor com metade dos valores: ${exercicio_8(minha_lista)}`)
