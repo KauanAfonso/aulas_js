aced95e906ddb2ae30e9d7faf24c92150acdc850
kauanAfonso <kauanafon3@gmail.com>
2025-04-04 07:50:10 -0300

    Exercicio 4

--- aula4/funcoes/lista_exercicios/exercicio4/index.js
+++ aula4/funcoes/lista_exercicios/exercicio4/index.js
@@ -1,4 +1,5 @@
 const verificar_idade = () =>{
     let idade = document.getElementById('idade').value
-    (idade >= 18)? alert('pode dirigir') : alert('Não pode dirigir')
+    idade >= 18 ? alert('pode dirigir') : alert('Não pode dirigir')
 }
+
