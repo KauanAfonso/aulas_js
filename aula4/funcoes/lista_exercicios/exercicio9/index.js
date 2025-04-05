dcbf398f68911983c346bff62c1abd77982594bf
KauanAfonso <kauanafon3@gmail.com>
2025-04-05 13:56:54 -0300

    Exercicio 9

--- aula4/funcoes/lista_exercicios/exercicio9/index.js
+++ aula4/funcoes/lista_exercicios/exercicio9/index.js
@@ -1,3 +1,29 @@
+
+//função que verifica dados do usuário
 const verificar_cadastro = ()=>{
 
+//Pegando informações
+let nome = document.getElementById('nome').value
+let endereco = document.getElementById('endereco').value
+let sexo = document.getElementsByName('sexo').value
+let telefone = document.getElementById('telefone').value
+let celular = document.getElementById('celular').value
+let email = document.getElementById('email').value
+
+//verificando se estão vazios
+if(nome == '' || endereco =='' || sexo == "" || telefone == "" || celular == "" || email == ""){
+    alert('Preencha todos os dados')
+}else{
+    alert("usuario cadastrado")
 }
+
+}
+
+let formulario = document.getElementById('form')
+
+//Ao formulario ser enviado chamar a função de verificar cadastro
+formulario.addEventListener('submit', (ev)=>{
+    ev.preventDefault()
+    verificar_cadastro()
+})
+
