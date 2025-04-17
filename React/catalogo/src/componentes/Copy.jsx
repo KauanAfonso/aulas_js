c765c3e5069c6e4a1d4508ee5d0c3a03810cfc18
kauanAfonso <kauanafon3@gmail.com>
2025-04-17 07:42:51 -0300

    removendo pasta de react

--- React/catalogo/src/componentes/Copy.jsx
+++
@@ -1,9 +0,0 @@
-import estilos from './Copy.module.css';
-
-export function Copy(){
-    return (
-        <div className={estilos.copyright}>
-            <p>&copy; 2025 Kauan Afonso. Todos os direitos reservados</p>
-        </div>
-    )
-}
